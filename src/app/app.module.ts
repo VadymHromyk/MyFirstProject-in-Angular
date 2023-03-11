ce45226540bb469a8edd19ef30a77c837e409c98
VadymHromyk <hromuk.vadim@gmail.com>
2023-03-11 21:24:02 +0200

    Refactor: deleted unusable components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,11 @@ import {BrowserModule} from '@angular/platform-browser'
 import {HttpClientModule} from '@angular/common/http'
 
 import {AppComponent} from './app.component'
-import {Test1Component} from './components/test1/test1.component'
-import {ParentComponent} from './parent/parent.component'
-import {ChildComponent} from './parent/child/child.component'
-import {Test2Component} from './components/test2/test2.component'
 import {TodosComponent} from './components/todos/todos.component'
 import {FormsComponent} from './components/forms/forms.component'
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    Test1Component,
-    ParentComponent,
-    ChildComponent,
-    Test2Component,
-    TodosComponent,
-    FormsComponent,
-  ],
+  declarations: [AppComponent, TodosComponent, FormsComponent],
   imports: [BrowserModule, FormsModule, HttpClientModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
